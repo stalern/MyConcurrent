cb65f2bb7719f76594f47101b410950f6d91ff2a
stalern <stalern2000@163.com>
2019-09-16 16:01:50 +0800

    test Idea commit github

--- src/MySynchronized.java
+++ src/MySynchronized.java
@@ -1,7 +1,27 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author stalern
  * @date 2019年9月16日15:49:34
+ * synchronized 的实现方式
  */
 public class MySynchronized {
-
+    private int a = 0;
+    private ReentrantLock lock = new ReentrantLock();
+    public void writer(){
+        lock.lock();
+        try{
+            a ++;
+        } finally {
+            lock.unlock();
+        }
+    }
+    public void reader(){
+        lock.lock();
+        try {
+            int i = a + 1;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
