caaf59736a7eca5f1f9943da829d6206239174de
stalern <stalern2000@163.com>
2019-09-16 17:16:20 +0800

    add main function

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author stalern
+ * @date 2019年9月16日17:14:18
+ * 主函数
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MySynchronized mySynchronized = new MySynchronized();
+        mySynchronized.writer();
+        mySynchronized.reader();
+    }
+}
