83548e1c6611c6ad391734d01c6c2746a08712b9
stalern <stalern2000@163.com>
2019-09-16 17:17:19 +0800

    add main function

--- src/MySynchronized.java
+++ src/MySynchronized.java
@@ -5,12 +5,12 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date 2019年9月16日15:49:34
  * synchronized 的实现方式
  */
-public class MySynchronized {
+class MySynchronized {
 
     private int a = 0;
     private ReentrantLock lock = new ReentrantLock();
 
-    public void writer(){
+    void writer(){
         lock.lock();
         try{
             a ++;
@@ -19,7 +19,7 @@ public class MySynchronized {
         }
     }
 
-    public void reader(){
+    void reader(){
         lock.lock();
         try {
             int i = a + 1;
